5a287f92171b7d9f7e7e71e8bf6cfb88f8cb58ff
Leonardo DaVchezt <davchezt@gmail.com>
2018-12-22 19:13:26 +0700

    ok fix

---
+++ public/electron.js
@@ -0,0 +1,95 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
+// Deep linked url
+let deeplinkingUrl
+
+require("update-electron-app")({
+  repo: "kitze/react-electron-example",
+  updateInterval: "1 hour"
+});
+
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+  app.quit();
+  return;
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Protocol handler for win32
+  // commandLine: An array of the second instance’s (command line / deep linked) arguments
+    if (process.platform == 'win32') {
+      // Keep only command line / deep linked arguments
+      deeplinkingUrl = commandLine.slice(1)
+    }
+    logEverywhere("app.makeSingleInstance# " + deeplinkingUrl);
+    mainWindow.webContents.send("harmony-url", deeplinkingUrl);
+
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  });
+
+  // Create mainWindow, load the rest of the app, etc...
+  app.on("ready", createWindow);
+
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+
+  app.on("activate", () => {
+    if (mainWindow === null) {
+      createWindow();
+    }
+  });
+
+  // Define custom protocol handler. Deep linking works on packaged versions of the application!
+  app.setAsDefaultProtocolClient('harmony');
+  // Protocol handler for osx
+  app.on('open-url', function (event, url) {
+    event.preventDefault();
+    deeplinkingUrl = url;
+    logEverywhere("open-url# " + deeplinkingUrl);
+    mainWindow.webContents.send("harmony-url", deeplinkingUrl);
+  });
+}
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680
+  });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.webContents.openDevTools({mode:'undocked'});
+
+  // Protocol handler for win32
+  if (process.platform == 'win32') {
+    // Keep only command line / deep linked arguments
+    deeplinkingUrl = process.argv.slice(1);
+  }
+  logEverywhere("createWindow# " + deeplinkingUrl);
+  mainWindow.webContents.send("harmony-url", deeplinkingUrl);
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+// Log both at dev console and at running node console instance
+function logEverywhere(s) {
+  console.log(s)
+  if (mainWindow && mainWindow.webContents) {
+      mainWindow.webContents.executeJavaScript(`console.log("${s}")`)
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,101 @@
 import React, { Component } from 'react';
+import Electron from 'electron';
+import Notifier from 'node-notifier';
+import path from 'path';
+import fs from 'fs';
 import logo from './logo.svg';
 import './App.css';
 
+// const { dialog } = Electron.remote;
+const { BrowserWindow, ipcMain, ipcRenderer, Notification, remote } = Electron;
+const { shell } = Electron;
+const { openExternal } = shell;
+const { app } = remote;
+
+const WindowsBalloon = require('node-notifier').WindowsBalloon;
+const WindowsToaster = require('node-notifier').WindowsToaster;
+
+const Platform = "windows";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  init() {
+    ipcRenderer.on("harmony-url", (event, message) => {
+      console.log(message);
+    });
+  }
+  componentWillMount() {
+    this.init();
+    fs.readFile(path.join(app.getAppPath(), "build/file.txt"), 'utf-8', function(err, buf) {
+      if (err) throw err;
+      console.log(buf.toString());
+    });
+    const notifierWindowsBalloon = new WindowsBalloon({
+      withFallback: false, // Try Windows Toast and Growl first?
+      customPath: path.join(app.getAppPath(), 'build/vendor/notifu/notifu.exe') // Relative/Absolute path if you want to use your fork of notifu
+    });
+    
+    notifierWindowsBalloon.notify({
+      title: "Harmony",
+      message: "Testing Notification",
+      sound: false, // true | false.
+      time: 10000, // How long to show balloon in ms
+      wait: false, // Wait for User Action against Notification
+      type: 'info' // The notification type : info | warn | error
+    }, function(error, response) {
+      console.log(response);
+    });
+  }
+  componentDidMount() {
+    this.init();
+    /*
+    dialog.showMessageBox(null, { type: 'info', title: 'Wow', message: 'MessageBox testing...', buttons: ['Ok', 'Retry', 'Cancel'] }, (ev) => {
+      console.log(ev);
+      if (ev === 1) dialog.showErrorBox('Error!', 'Testing error box');
+    });
+    // dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] });
+    */
+    let notifier = new WindowsToaster({
+      withFallback: false,
+      customPath: path.join(app.getAppPath(), 'build/vendor/snoreToast/SnoreToast.exe')
+    });
+
+    notifier.notify({
+      title: 'Harmony',
+      message: 'Hello. This is a longer text\nWith "some" newlines.',
+      wait: false,
+      icon: 'file://' + path.join(app.getAppPath(), 'build/coulson.jpg'),
+      sound: true
+    }, function(err, data) {
+      // Will also wait until notification is closed.
+      console.log('Waited');
+      console.log(err, data);
+    });
+
+    notifier.on('timeout', function() {
+      console.log('notifier Timed out!');
+    });
+
+    notifier.on('click', function() {
+      console.log('notifier Clicked!');
+    });
+  }
+
+  openLink(event) {
+    let link = event.target.getAttribute('href');
+    if (link) {
+      openExternal(link, {}, (err) => {
+        if (err) {
+          throw err;
+        }
+        shell.beep();
+        console.log("visit: ", link);
+      })
+      event.preventDefault();
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -16,6 +109,7 @@ class App extends Component {
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
+            onClick={this.openLink}
           >
             Learn React
           </a>
