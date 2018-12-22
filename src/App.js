76ed3af89b242a4181ce0bc2f0197f5cdbd59dc0
Leonardo DaVchezt <davchezt@gmail.com>
2018-12-23 03:19:56 +0700

    y

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ const assetsLocation = process.env.NODE_ENV === "development" ?
 class App extends Component {
   constructor(props) {
     super(props);
-	
-	  this.state = {
+
+    this.state = {
       inDevelopment: true
-	  }
+    }
   }
 
   init() {
@@ -38,16 +38,16 @@ class App extends Component {
           console.log(router);
         }
       }
-      
+
       console.log(message);
     });
   }
 
   componentWillMount() {
     if (process.env.NODE_ENV !== "development") {
-	    this.setState({inDevelopment: false});
-	  }
-	
+      this.setState({ inDevelopment: false });
+    }
+
     notifierWindowsBalloon.notify({
       title: "Harmony",
       message: "Testing Notification",
@@ -55,22 +55,22 @@ class App extends Component {
       time: 10000,
       wait: false,
       type: 'info'
-    }, function(error, response) {
+    }, function (error, response) {
       if (response) console.log(response);
-	    if (error) console.log(error);
+      if (error) console.log(error);
     });
-	
-	  notifierWindowsBalloon.on('timeout', function() {
+
+    notifierWindowsBalloon.on('timeout', function () {
       console.log('notifier Timed out!');
     });
 
-    notifierWindowsBalloon.on('click', function() {
+    notifierWindowsBalloon.on('click', function () {
       console.log('notifier Clicked!');
     });
   }
   componentDidMount() {
     this.init();
-    
+
     Notifier.notify({
       title: 'Harmony',
       message: 'Hello. This is a longer text\nWith "some" newlines.',
@@ -78,16 +78,16 @@ class App extends Component {
       wait: false,
       icon: assetsLocation + "/img/coulson.jpg",
       sound: true
-    }, function(err, data) {
+    }, function (err, data) {
       if (err) console.log(err);
-	    if (data) console.log(err);
+      if (data) console.log(err);
     });
 
-    Notifier.on('timeout', function() {
+    Notifier.on('timeout', function () {
       console.log('notifier Timed out!');
     });
 
-    Notifier.on('click', function() {
+    Notifier.on('click', function () {
       console.log('notifier Clicked!');
     });
   }
