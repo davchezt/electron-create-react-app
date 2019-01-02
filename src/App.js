59d6cc37e7c4ae202e766a76b057bf8cc0dcc825
Leonardo DaVchezt <davchezt@gmail.com>
2019-01-03 01:45:44 +0700

    Add react-router working with deep-linking

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Electron from 'electron';
 import Notifier from 'electron-node-notifier';
 import path from 'path';
+import { NavLink, Route, Link, HashRouter, Switch } from "react-router-dom";
 
 import logo from './logo.svg';
 import './App.css';
@@ -18,6 +19,19 @@ const assetsLocation = process.env.NODE_ENV === "development" ?
   path.resolve(path.join(__dirname, "../public/assets")) :
   path.join(app.getAppPath().replace('app.asar', 'app.asar.unpacked/assets'));
 
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <Link to="/">Home</Link>
+  </div>
+);
+const Users = () => (
+  <div>
+    <h2>Users</h2>
+    <Link to="/">Home</Link>
+  </div>
+);
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -32,13 +46,12 @@ class App extends Component {
       if (message.length !== 0) {
         if (message[0].length > 1) {
           let urlPath = message[0].split(":/")[1];
-          urlPath = urlPath.toString().substring(1, urlPath.length - 1);
-          console.log("/" + urlPath);
-          let router = urlPath.split('/');
-          console.log(router);
+          urlPath = urlPath.toString().substring(0, urlPath.length - 1);
+          console.log(urlPath);
+          window.location.hash = urlPath;
         }
+        console.log(event)
       }
-
       console.log(message);
     });
   }
@@ -47,7 +60,7 @@ class App extends Component {
     if (process.env.NODE_ENV !== "development") {
       this.setState({ inDevelopment: false });
     }
-
+    /*
     notifierWindowsBalloon.notify({
       title: "Harmony",
       message: "Testing Notification",
@@ -67,10 +80,11 @@ class App extends Component {
     notifierWindowsBalloon.on('click', function () {
       console.log('notifier Clicked!');
     });
+    */
   }
   componentDidMount() {
     this.init();
-
+    /*
     Notifier.notify({
       title: 'Harmony',
       message: 'Hello. This is a longer text\nWith "some" newlines.',
@@ -90,6 +104,7 @@ class App extends Component {
     Notifier.on('click', function () {
       console.log('notifier Clicked!');
     });
+    */
   }
 
   openLink(event) {
@@ -107,26 +122,55 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          {!this.state.inDevelopment && <a className="App-link" href="harmony://test">test deep-link</a>}
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={this.openLink}
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={styles}>
+        <HashRouter>
+          <Switch>
+            <Route exact path="/" component={() => (
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.<br />
+                    <NavLink exact to="/users" activeClassName="selected">Users</NavLink>
+                  </p>
+                  {!this.state.inDevelopment && <a className="App-link" href="harmony://about">test deep-link</a>}
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={this.openLink}
+                  >
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            )} />
+            <Route path="/about" component={About} />
+            <Route path="/users" component={Users} />
+            <Route path="/dua" component={() => (
+              <div>
+                <h2>DUA</h2>
+                <Link to="/">Home</Link>
+              </div>
+            )} />
+            <Route component={() => (
+              <div>
+                <h1>404</h1>
+                <Link to="/">Home</Link>
+              </div>
+            )} />
+          </Switch>
+        </HashRouter>
       </div>
+      /**/
     );
   }
 }
 
 export default App;
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
